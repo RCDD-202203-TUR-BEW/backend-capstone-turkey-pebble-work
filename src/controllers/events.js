7f79ca3b10ff2db24f518eeee44b005263ca248a
Mehmet Baki <mehmetbaki.123@gmail.com>
2022-08-02 00:03:44 +0300

    add createEvent final version

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,48 +1,38 @@
 const Event = require('../models/event')
-const uploadImage = require('../db/storage')
-
+const { uploadImage } = require('../db/storage')
+const { getFileExtension } = require('../utility/utils');
 
 const createEvent = (req, res) => {
 
     try {
-
+        const imageUrl = await uploadImage(
+            req.file,
+            `${COVER_IMAGE_DIR}/${Date.now()}.${getFileExtension(
+                req.file.originalname
+            )}`
+        );
         const event = await new Event({
-
-            puplisherId: req.user.id,
+            publisherId: req.user.id,
             title: req.body.title,
             content: req.body.content,
             coverImage: imageUrl,
             date: req.body.date,
             category: req.body.category,
-
             address: {
                 city: req.body.city,
                 country: req.body.country,
                 addressLine: req.body.addressLine
             },
-
             location: {
                 lat: req.body.lat,
                 log: req.body.long
             }
-
-
         }).save()
-
         res.status(201).json({ message: 'Event succesfully created' })
-
     } catch (err) {
-        res.status(400).json({ message: 'Bad Request' })
+        res.status(400).json({ message: err.message })
     }
-
-
-
-
-
-
-
-}
-
+};
 
 module.exports = {
     createEvent
--- src/routes/evets.js
+++ src/routes/evets.js
@@ -5,15 +5,12 @@ const eventController = require('../controllers/events')
 const { MAX_IMAGE_SIZE } = require('../utility/variables');
 const { CREATE_EVENT_VALIDATION_RULES, handleValidation } = require('../utility/validation')
 
-
 const multer = Multer({
     storage: Multer.memoryStorage(),
     limits: {
         fileSize: MAX_IMAGE_SIZE,
     },
 });
-
-
 
 router.post('/events', multer.single('coverImage'), CREATE_EVENT_VALIDATION_RULES, handleValidation, eventController.createEvent)
 
