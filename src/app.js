5e53337001262cd2b86dd23418514583af854692
Muhammed Sihebi <58932402+muhammed-shihebi@users.noreply.github.com>
2022-08-02 05:08:33 +0300

    add swagger docs for the auth endpoints Fixes #34

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const { encryptCookieNodeMiddleware } = require('encrypt-cookie');
 const connectToMongo = require('./db/connection');
 const authRouter = require('./routes/auth');
 const { authMiddleware } = require('./middleware');
-const { connectToStorage } = require('./db/storage');
 
 const app = express();
 const port = process.env.PORT;
@@ -16,6 +17,35 @@ app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'PebbleWork API documentation with Swagger',
+            version: '0.1.0',
+            description: 'This is a API application made with Express',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'PebbleWork',
+                url: 'https://github.com/RCDD-202203-TUR-BEW/backend-capstone-turkey-pebble-work',
+                email: process.env.EMAIL,
+            },
+        },
+        servers: [
+            {
+                url: process.env.BASE_URL,
+            },
+        ],
+        host: process.env.BASE_URL,
+    },
+    apis: ['./src/docs/**/*.yaml'],
+};
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(authMiddleware);
 
