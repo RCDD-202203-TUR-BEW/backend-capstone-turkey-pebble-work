5c74fc7ade11450df4d9406101ea0d0f32cb8c26
Rama ALSHABAN <50273459+ramaalshaban@users.noreply.github.com>
2022-08-05 12:33:58 +0300

    Update variables.js

--- src/utility/variables.js
+++ src/utility/variables.js
@@ -102,8 +102,33 @@ const CATEGORIES = [
     'Youth',
     'Justice',
 ];
-
+const OPTIONS = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'PebbleWork API documentation with Swagger',
+            version: '0.1.0',
+            description: 'This is a API application made with Express',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'PebbleWork',
+                url: 'https://github.com/RCDD-202203-TUR-BEW/backend-capstone-turkey-pebble-work',
+            },
+        },
+        servers: [
+            {
+                url: process.env.BASE_URL,
+            },
+        ],
+        host: process.env.BASE_URL,
+    },
+    apis: ['./src/docs/*/.yaml'],
+};
 module.exports = {
     CITIES,
     CATEGORIES,
+    OPTIONS,
 };
