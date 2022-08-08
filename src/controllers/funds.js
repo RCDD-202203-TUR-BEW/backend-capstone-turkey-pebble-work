97dc753998c148b8d3dfe2826758c7450f01229f
Mehmet Baki <mehmetbaki.123@gmail.com>
2022-08-08 23:50:00 +0300

    add createfund function

---
+++ src/controllers/funds.js
@@ -0,0 +1,28 @@
+const Fund = require("../models/fund")
+
+const createFund = async (req, res) => {
+    try {
+        const fund = await Fund.create({
+            publisherId: req.user.id,
+            title: req.body.title,
+            content: req.body.content,
+            deadline: req.body.deadline,
+            targetFund: req.body.targetFund,
+            category: req.body.category,
+            address: {
+                city: req.body.city,
+                country: req.body.country,
+                addressLine: req.body.addressLine
+            },
+        })
+        res.status(201).json(fund)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+    }
+}
+
+module.exports = {
+    createFund
+}
+ 
