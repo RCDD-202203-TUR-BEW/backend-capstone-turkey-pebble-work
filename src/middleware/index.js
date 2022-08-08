74c34d12b8011fcc489557d48239aacf2b965bc7
Muhammed Sihebi <58932402+muhammed-shihebi@users.noreply.github.com>
2022-08-08 21:34:48 +0300

    merge

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -7,8 +7,10 @@ const publicRoutes = [
     '/api/auth/image',
     // we can't use /api/auth/verify/:id/:token directly
     // this is a regex, so it will match any path similar to /api/auth/verify/324/123456789
+    // https://forbeslindesay.github.io/express-route-tester/
     /^\/api\/auth\/verify\/(?:([^/]+?))\/(?:([^/]+?))\/?$/i,
     { url: '/api/event/', methods: ['GET'] },
+    { url: /^\/api\/fund\/(?:([^/]+?))\/donate\/?$/i, methods: ['POST'] },
 ];
 
 const authMiddleware = jwt({
--- src/routes/funds.js
+++ src/routes/funds.js
@@ -24,4 +24,3 @@ router.get(
     fundsController.getFunds
 );
 module.exports = router;
-
