4a18bb059807d050c9b20cbd34a933aa814894e0
Mehmet Baki <mehmetbaki.123@gmail.com>
2022-08-12 00:46:21 +0300

    update createFund

--- src/controllers/funds.js
+++ src/controllers/funds.js
@@ -2,27 +2,27 @@ const Fund = require("../models/fund")
 
 const createFund = async (req, res) => {
     try {
+        const { title, content, deadline, targetFund, categories, address } = req.body
         const fund = await Fund.create({
             publisherId: req.user.id,
-            title: req.body.title,
-            content: req.body.content,
-            deadline: req.body.deadline,
-            targetFund: req.body.targetFund,
-            categories: req.body.categories,
+            title: title,
+            content: content,
+            deadline: deadline,
+            targetFund: targetFund,
+            categories: categories,
             address: {
-                city: req.body.city,
-                country: req.body.country,
-                addressLine: req.body.addressLine
+                city: address.city,
+                country: address.country,
+                addressLine: address.addressLine
             },
         })
         res.status(201).json(fund)
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: err.message })
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 }
 
 module.exports = {
     createFund
 }
- 
